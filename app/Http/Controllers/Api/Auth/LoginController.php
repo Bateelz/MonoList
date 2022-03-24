e7aabbb97d2eef0cc96fbd4dba6b489db6bbe940
ahmadBateelz <ahmadqawasmeh@bateelz.com>
2022-03-24 23:38:42 +0300

    Updated

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class LoginController extends Controller
@@ -34,26 +35,69 @@ class LoginController extends Controller
         return $this->errorResponse("Wrong Email Or Password");
     }
 
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         $this->validate($request, [
-            'fullname'=>'required',
+            'fullname' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required',
         ]);
-        $user=new User();
-        $user->fullname=$request->fullname;
-        $user->email=$request->email;
-        $user->password=Hash::make($request->password);
+        $user = new User();
+        $user->fullname = $request->fullname;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
         $user->save();
         $token = JWTAuth::fromuser($user);
-        $data=[
-            'user_id'=>$user->id,
-            'email'=>$user->email,
+        $data = [
+            'user_id' => $user->id,
+            'email' => $user->email,
         ];
         Mail::to($request->email)->send(new ActiveUser($data));
         return $this->successResponse([
             "user" => $user,
             "token" => $token
         ]);
+    }
+
+    public function login_by_social(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'fullname' => 'required',
+            'email' => 'required',
+            'login_by' => 'required',
+            'login_id' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return $this->errorResponse($validator->errors()->all(), 422);
+        }
+
+        $fullname = $request->fullname;
+        $email    = $request->email;
+        $password = "password";
+        $login_by = $request->login_by;
+        $login_id = $request->login_id;
+
+        $user = User::Where('login_by', $login_by)->where('email',$email)->Where('login_id', $login_id)->first();
+        if ($user) {
+            $token = JWTAuth::fromUser($user);
+            return $this->successResponse(["user" => $user,"token" => $token]);
+        } else {
+            $user = User::create([
+            'fullname' => $fullname,
+            'email' => $email,
+            'password' => $password,
+            'login_by' => $login_by,
+            'login_id' => $login_id,
+        ]);
+            $token = JWTAuth::fromUser($user);
+            return $this->successResponse(["user" => $user,"token" => $token]);
+        }
+        return $this->errorResponse('error');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return $this->successResponse(null, "Success Logout");
     }
 }
